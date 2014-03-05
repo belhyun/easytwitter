645bd2be6c0d985aa6c5fe50a7b6eea40f649792
belhyun <belhyun@gmail.com>
2014-03-05 20:53:09 +0900

    update

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,25 @@
+class CategoriesController < ApplicationController
+  before_action :set_category
+  def show
+    @tweets = Kaminari.paginate_array(Category.find_by(:id => params[:id].to_i).tweets.desc(:score)).page(params[:page]).per(10)
+    gon.tweets = @tweets
+    gon.type = 1
+  end
+
+  def recent
+    @tweets = Kaminari.paginate_array(Category.find_by(:id => params[:id].to_i).tweets.desc(:created_at)).page(params[:page]).per(10)
+    gon.tweets = @tweets
+    gon.type = 2
+  end
+
+  def people
+    @tweets = Kaminari.paginate_array(Category.find_by(:id => params[:id].to_i).tweets.desc(:name)).page(params[:page]).per(10)
+    gon.tweets = @tweets
+    gon.type = 3
+  end
+
+  private
+  def set_category
+    @category = Category.find_by(:id => params[:id].to_i)
+  end
+end
